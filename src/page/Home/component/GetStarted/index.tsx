21e2e561a6299193604ee9cc7e2189cbd1250761
Philip Chen <lsps9150414@gmail.com>
2021-05-18 15:39:23 +0800

    refactor: replace Link with ExternalLink

--- src/page/Home/component/GetStarted/index.tsx
+++ src/page/Home/component/GetStarted/index.tsx
@@ -1,5 +1,7 @@
+import { Button, Divider, HStack, Heading, Stack, Text } from "@chakra-ui/react"
+
+import { ExternalLink } from "component/ExternalLink"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { Stack, Heading, HStack, Divider, Button, Text, Link } from "@chakra-ui/react"
 
 function GetStarted() {
     return (
@@ -8,19 +10,19 @@ function GetStarted() {
                 <Heading size="md">About</Heading>
                 <Text>
                     This is a lite version of{" "}
-                    <Link color="blue.500" href="https://perp.exchange" isExternal>
+                    <ExternalLink color="blue.500" href="https://perp.exchange" isExternal>
                         Perpetual Protocol <ExternalLinkIcon mx="2px" />
-                    </Link>{" "}
+                    </ExternalLink>{" "}
                     UI with essential features. Note that you need to manually switch to <strong>xDai network</strong>{" "}
                     to trade.
                 </Text>
                 <HStack>
-                    <Link color="blue.500" href="https://github.com/perpetual-protocol/perp-lite" isExternal>
+                    <ExternalLink color="blue.500" href="https://github.com/perpetual-protocol/perp-lite" isExternal>
                         Github <ExternalLinkIcon mx="2px" />
-                    </Link>{" "}
-                    <Link color="blue.500" href="https://discord.com/invite/mYKKRTn" isExternal>
+                    </ExternalLink>{" "}
+                    <ExternalLink color="blue.500" href="https://discord.com/invite/mYKKRTn" isExternal>
                         Discord <ExternalLinkIcon mx="2px" />
-                    </Link>
+                    </ExternalLink>
                 </HStack>
             </Stack>
             <Divider />

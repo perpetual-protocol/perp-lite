f7cfdd3c7dde4a236b7265343e6eb557f2b9c292
Leo <leolin@me.com>
2021-05-21 11:59:47 +0800

    feat: switch to "trade" tab if connected

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,11 +1,26 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import GetStarted from "./component/GetStarted"
 import Position from "./component/Position"
 import TradeComponent from "./component/Trade"
+import { User } from "container/user"
 
 const Home = () => {
     const [tabIndex, setTabIndex] = useState(0)
+
+    // Need review: 偵測到 connect wallet 以後，設定  setTabIndex(1)，才不必每次都看到 get started
+
+    const {
+        state: { address },
+    } = User.useContainer()
+
+    useEffect(() => {
+        if (address) {
+            setTabIndex(1)
+        } else {
+            setTabIndex(0)
+        }
+    }, [address])
 
     return (
         <Tabs size="md" mt={5} index={tabIndex} onChange={index => setTabIndex(index)} variant="enclosed">
